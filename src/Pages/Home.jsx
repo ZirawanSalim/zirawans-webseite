b4e6f9496dbad72df4d87d1ab80556869dfe10ee
Zirawan salim <zirawan@hotmail.com>
2025-11-14 09:56:03 +0100

    MeinData.json geupdatet

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,6 @@
 
 import AboutMe from '../components/AboutMe.jsx';
 import NavBar from '../components/NavBar.jsx';
- 
 
 export default function Home() {
     return (
--- src/Pages/Projekte.jsx
+++ src/Pages/Projekte.jsx
@@ -1,15 +1,15 @@
 
 import NavBar from '../components/NavBar.jsx';
 import { Link } from 'react-router-dom';
-import MeinProjekte from '../components/MeinProjekte.jsx';  
+import MeinProjekte from '../components/MeinProjekte.jsx';
 
 export default function ProjektePage() {
     return (
         <div>
-        <NavBar />  
-     <h3>Projekte Page</h3>
-         <Link to="/">home</Link>
-         <MeinProjekte />
+            <NavBar />
+            <MeinProjekte />
+            <h3>Projekte Page</h3>
+            <Link to="/">home</Link>
 
         </div>
     )
